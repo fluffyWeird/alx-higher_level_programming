884da53a50b63370e263a12f6a817fbc0ab14d5d
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-11 22:50:24 +0300

    done

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
-=======
 number = 3.14159
-print("Float: {:.2f}".format(number))
->>>>>>> 76c969c1b613f97b60e232815ab2e678652bfd2a
+print(f"Float: {number:.2f}")
--- 0x00-python-hello_world/main.py
+++ 0x00-python-hello_world/main.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-print("Best School")
-=======
 #!/usr/bin/python3
-print("Holberton School")
->>>>>>> 76c969c1b613f97b60e232815ab2e678652bfd2a
+print("Best School")
