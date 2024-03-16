d5f0ad6906be24eaaec05b8b1d0b712aca96ac9f
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-16 15:05:47 +0300

    done

---
+++ 0x00-python-hello_world/pypy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+age = eval(input("Input your age"))
+if age < 5:
+	print("Tooo young")
+elif (age > 5) and (age < 10):
+	print("good enough for elementary")
+else:
+	print("she is good")
+
--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,4 @@
 import add_0 as add
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add.add(a, b) ))
+print('{:d} + {:d} = {:d}'.format(a, b, add.add(a, b)))
