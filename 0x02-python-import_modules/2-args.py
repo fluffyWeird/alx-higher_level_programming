26b3fa3b5979895371406123e8aeb1118d68386d
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-17 22:10:43 +0300

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-argc = len(sys.argv) - 1
-print('{} {}:'.format(argc, sys.argv[argc]))
-for i in range(argc):
-    j = i + 1
-    print('{}: {}'.format(j, sys.argv[j]))
+if __name__ == '__main__':
+    argc = len(sys.argv) - 1
+    print('{} {}:'.format(argc, sys.argv[argc]))
+    for i in range(argc):
+        j = i + 1
+        print('{}: {}'.format(j, sys.argv[j]))
