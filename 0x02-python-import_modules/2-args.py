2058beb1a5648b8b16461751fec170301d6636f0
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-17 22:17:01 +0300

    doen

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 import sys
 if __name__ == '__main__':
     argc = len(sys.argv) - 1
-    print('{} {}:'.format(argc, sys.argv[argc]))
-    for i in range(argc):
-        j = i + 1
-        print('{}: {}'.format(j, sys.argv[j]))
+    if argc > 0:
+        print('{} arguments:'.format(argc))
+        for i in range(argc):
+            j = i + 1
+            print('{}: {}'.format(j, sys.argv[j]))
+    elif argc == 0:
+        print('0 arguments.')
