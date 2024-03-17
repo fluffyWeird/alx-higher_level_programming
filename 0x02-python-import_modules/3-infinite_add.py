a97998d4cb9eae7c4bcdc71c07ac212ce6b71e20
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-17 22:42:43 +0300

    I think it is fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv) - 1
-sum = 0
-for i in range(argc):
-    j = i + 1
-    asrgint = int(sys.argv[j])
-    sum += asrgint
+if __name__ == '__main__':
+    import sys
+    argc = len(sys.argv) - 1
+    sum = 0
+    for i in range(argc):
+        j = i + 1
+        asrgint = int(sys.argv[j])
+        sum += asrgint
 print(sum)
