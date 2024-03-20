d242714b6dab1f96a4fe867c80a24541353ef33f
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-20 13:25:09 +0300

    done

--- 0x03-python-data_structures/pypy.py
+++ 0x03-python-data_structures/pypy.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-list2= ['ababa', 23, 'ametu', 'new']
-print(list2)
-sentence=''
-for i in range(len(list2)):
-   sentence = sentence + str(list2[i]) + ' '
-print('{}'.format(sentence))
+#let's create our first list
+# x = list()
+# x = [1, 2, 3, 4, 5]
+# y=[6, 7,  8, 9]
+# x.extend(y)
+# print(x)
+
+##And now let's create a Tupil
+# x = ()
+# x= 1, 2, 3
+# x[1]=2
+# print(x)
+
+##And now we create a set
+# x={1,2,3}
+# y=[4,5,6]
+# z=set(y)
+# print(z)
+
+##And now we create a Dictionary
+x={'name':'Abebe', 'Age':23, 'Career': 'gov.employee'}
+print(x['name'], end='')
+#to access the specific dictionary Entry we use
+print(x['Age'])
