ad55d575caed11691688e59a3cf81b9485642b07
fluffyweird <ets027912feng@aastu.edu.et>
2024-03-21 13:09:35 +0300

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_str += char
-    return new_str
+    for i in my_string:
+        if my_string[i] == 'c':
+            my_string[i] = ''
+    return str12
