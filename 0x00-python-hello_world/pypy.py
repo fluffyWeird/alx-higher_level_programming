7a47cb989142d46444ebe9e72615bc660baa7c23
fluffyweird <ets027912feng@aastu.edu.et>
2024-04-03 22:24:15 +0300

    done

--- 0x00-python-hello_world/pypy.py
+++ 0x00-python-hello_world/pypy.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-age = eval(input("Input your age"))
-if age < 5:
-	print("Tooo young")
-elif (age > 5) and (age < 10):
-	print("good enough for elementary")
-else:
-	print("she is good")
-
+for i in range (2, 10, 2):
+  print(i, end=" ")
