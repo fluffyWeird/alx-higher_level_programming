25c6c86846a1969f77b948b86fb98e22e9d5ab9a
fluffyweird <ets027912feng@aastu.edu.et>
2024-05-04 09:02:22 +0300

    added some code where you can send message directly to the server but removed the bits where you can give sequence number to messages ... I think this is necessary if you wish to print out the message data onto a txt file

--- Ruth_Ass/server.py
+++ Ruth_Ass/server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3 
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -7,13 +8,13 @@ expected_seq_num = 0
 
 while True:
     data, addr = server_socket.recvfrom(1024)
-    received_seq_num = int(data.decode().split()[0])
-    print(f"Received message: {data}, Sequence Number: {received_seq_num}")
+    # received_seq_num = int(data.decode().split()[0])
+    # print(f"Received message: {data}, Sequence Number: {received_seq_num}")
+    print(f"{data.decode()}")
+    # if received_seq_num == expected_seq_num:
+    #     ack_message = f"ACK {received_seq_num}".encode()
+    #     expected_seq_num += 1
+    # else:
+    #     ack_message = f"ACK {expected_seq_num - 1}".encode()
     
-    if received_seq_num == expected_seq_num:
-        ack_message = f"ACK {received_seq_num}".encode()
-        expected_seq_num += 1
-    else:
-        ack_message = f"ACK {expected_seq_num - 1}".encode()
-    
-    server_socket.sendto(ack_message, addr)
+    # server_socket.sendto(ack_message, addr)
